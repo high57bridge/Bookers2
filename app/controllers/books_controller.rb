c722a556796932e7f05f1e42b973166bf931f514
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-14 12:25:17 +0000

    エラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,26 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
-    @books = Book.all
   end
 
   def create
+    # book = Book.find(params[:id])
     @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render:index
+    end
+    
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
   end
   
   def index
+    @book = Book.new
+    @books = Book.all
+    book = Book.find(params[:id])
   end
   
   def show
@@ -23,8 +31,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   def destroy
-    book = Book.destroy(params[:id])
+    book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit]
   
+  patch 'books/:id' => 'books#update', as: 'update_book'
   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
