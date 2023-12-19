1e6a518312ceac45745bc61e34ad52c989e57ac9
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-19 10:01:36 +0000

    icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
  
  def index 
-   @books = Book.find(params[:id])
+  # @books = Book.find(params[:id])
    @book = Book.new
    @user = current_user
  end
@@ -22,6 +24,7 @@ end
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -44,5 +47,12 @@ end
   
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
